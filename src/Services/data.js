662ffff30b69fa5acf981b51f72bee11b27c2340
Jesse Santos <jessesantos02001.JS@gmail.com>
2022-06-24 13:54:41 -0500

    All new Files

---
+++ src/Services/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+      src: "",
+      title: "w",
+    },
+    {
+      src: "",
+      title: "",
+    },
+    {
+      src: "",
+      title: "",
+    },
+    {
+      src: "",
+      title: "",
+    },
+    {
+      src: "",
+      title: "",
+    },
+  ];
+  
+  export default data;
